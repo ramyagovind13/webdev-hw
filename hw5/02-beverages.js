c02335d24acf1d86a0a10d86536fceef7b94e021
ramyagovind <ramya3@pdx.edu>
2022-11-11 15:57:01 -0800

    Homework-5 assignments completed

--- hw5/02-beverages.js
+++ hw5/02-beverages.js
@@ -1,10 +1,71 @@
 const url =
-  'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+  "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
-let app = document.querySelector('#results');
+let app = document.querySelector("#results");
+const loading = document.querySelector("#loading");
 
-const fetchData = (url) => {
-  // Add your code here
+const addDataToDOM = (item) => {
+  let div = document.createElement("div");
+  div.style.width = "225px";
+  div.style.padding = "10px 10px 10px 10px";
+  div.style.border = "5px solid white";
+  div.style.maxHeight = "400px";
+  app.append(div);
+
+  let imgdiv = document.createElement("div");
+  imgdiv.id = "imgdiv";
+  imgdiv.padding = "5px 5px 5px 5px";
+  imgdiv.innerHTML =
+    "<img src =" +
+    item.strDrinkThumb +
+    " height = 200px width = 200px alt = " +
+    item.idDrink +
+    "> </img>";
+  div.append(imgdiv);
+
+  let itemName = document.createElement("div");
+  itemName.innerHTML = item.strDrink;
+  itemName.style.textAlign = "center";
+  itemName.style.fontSize = "1.3em";
+  itemName.style.color = "black";
+  div.append(itemName);
+
+  div.addEventListener("mouseover", function (event) {
+    div.style.backgroundColor = "purple";
+    itemName.style.color = "white";
+  });
+  div.addEventListener("mouseout", function (event) {
+    div.style.backgroundColor = "transparent";
+    itemName.style.color = "black";
+  });
+
+  app.style.display = "flex";
+  app.style.flexDirection = "row";
+  app.style.flexWrap = "wrap";
+  app.style.justifyContent = "center";
+
+  div.style.display = "flex";
+  div.style.flexDirection = "column";
+  div.style.flexWrap = "wrap";
+  div.style.textAlign = "center";
 };
+
+async function fetchData(url) {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    data.drinks.forEach((elem) => {
+      addDataToDOM(elem);
+    });
+  } catch (error) {
+    console.log(error);
+    let para = document.createElement("p");
+    para.textContent = `An error occured. Please try again.`;
+    app.append(para);
+  } finally {
+    app.removeChild(loading);
+  }
+}
 
 fetchData(url);
--- hw5/03-beverages.js
+++ hw5/03-beverages.js
@@ -1,6 +1,75 @@
 $(document).ready(function () {
   const url =
-    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
-  // Add your code here
+  const addDataToDOM = (item) => {
+    $("#results")
+      .append(
+        $('<div id = "maindiv">')
+          .css({
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            width: "225px",
+            padding: "10px 10px 10px 10px",
+            margin: "0 auto",
+            border: "5px solid white",
+            maxHeight: "400px",
+          })
+          .append(
+            $('<div id = "imgdiv">').append(
+              $(
+                "<img src =" +
+                  item.strDrinkThumb +
+                  " height = 200px width = 200px alt = " +
+                  item.idDrink +
+                  "> </img>"
+              )
+            )
+          )
+          .append(
+            $('<div id = "namediv">')
+              .css({
+                fontSize: "1.3em",
+                color: "black",
+                textAlign: "center",
+              })
+              .append(item.strDrink)
+          )
+      )
+      .css({
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      });
+
+    $("#maindiv").hover(function () {
+      $(this).css("background-color", "purple");
+    });
+  };
+
+  const fetchData = (url) => {
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: (data) => {
+        console.log(data);
+        data.drinks.forEach((item) => {
+          addDataToDOM(item);
+        });
+      },
+      error: (error) => {
+        console.log(error);
+        $("#results")
+          .append("<div>")
+          .text(`An error occured. Please try again.`);
+      },
+      complete: () => {
+        $("#loading").remove();
+      },
+    });
+  };
+
+  fetchData(url);
 });
